b8770ab545971bbba5caa7327e903a2fc6454d51
Joshua <gjoshua2016@gmail.com>
2026-07-06 15:37:55 +0530

    final login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -33,66 +33,102 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2   style={{
-        color: "blue",
-        border: "1px solid black",
-        borderRadius: "10px",
-        backgroundColor: "lightgray",
-        padding: "10px",
-        textAlign: "center",
-        justifyContent: "center",
-       }}>Login</h2>
+    
+   <div
+  style={{
+    width: "350px",
+    margin: "80px auto",
+    padding: "40px",
+    border: "2px solid #333",
+    borderRadius: "15px",
+    background: "white",
+    textAlign: "center",
+  }}
+>
+<h2
+  style={{
+    color: "blue",
+    border: "1px solid black",
+    borderRadius: "10px",
+    backgroundColor: "lightgray",
+    padding: "12px",
+    marginBottom: "25px",
+  }}
+>
+  Login
+</h2>
        
-      <input
-        name="email"
-        placeholder="Email"
-        onChange={handleChange}
-        style={{
-            margin: "10px",
-            padding: "5px",
-            borderRadius: "5px",
-            border: "1px solid gray",
-          }}    
-      />
-      
+ <input
+  name="email"
+  placeholder="Email"
+  onChange={handleChange}
+  style={{
+    width: "100%",
+    padding: "12px",
+    marginBottom: "20px",
+    borderRadius: "8px",
+    border: "1px solid gray",
+    boxSizing: "border-box",
+  }}
+/>
 
       <br /><br />
 
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-         style={{
-            margin: "10px",
-            padding: "5px",
-            borderRadius: "5px",
-            border: "1px solid gray",
-          }}  
-      />
+<input
+  type="password"
+  name="password"
+  placeholder="Password"
+  onChange={handleChange}
+  style={{
+    width: "100%",
+    padding: "12px",
+    marginBottom: "20px",
+    borderRadius: "8px",
+    border: "1px solid gray",
+    boxSizing: "border-box",
+  }}
+/>
 
       <br /><br />
 
-      <button onClick={login}
-      style={{
-        fontWeight: "bold",
-        color: "blue",
-        cursor: "pointer",
-      }}>Login</button>
+<button
+  onClick={login}
+  style={{
+    width: "100%",
+    padding: "12px",
+    backgroundColor: "#2563eb",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    marginBottom: "15px",
+  }}
+>
+  Login
+</button>
 
        <br /><br />
 
-    <button onClick={() => navigate("/register")}
-        style={{
-          fontWeight: "bold",
-          color: "blue",
-          cursor: "pointer",
-        }}>
-      Register
-    </button>
+<button
+  onClick={() => navigate("/register")}
+  style={{
+    width: "100%",
+    padding: "12px",
+    backgroundColor: "#10b981",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontWeight: "bold",
+    cursor: "pointer",
+  }}
+>
+  Register
+</button>
     
   </div>
+  
+  
   );
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,28 +18,77 @@ function Register() {
         }
     };
     return (
-        <div>
-            <h2>Register</h2>
-            <input
-                name="username"
-                placeholder="Username"
-                onChange={handleChange}
+          
+   <div
+  style={{
+    width: "350px",
+    margin: "80px auto",
+    padding: "40px",
+    border: "2px solid #333",
+    borderRadius: "15px",
+    background: "white",
+    textAlign: "center",
+  }}
+>
+ <h2>Register</h2>
+    <input
+     name="username"
+     placeholder="Username"
+     onChange={handleChange}
+     style={{
+     width: "100%",
+     padding: "12px",
+     marginBottom: "20px",
+     borderRadius: "5px",
+     border: "1px solid gray",
+     boxSizing: "border-box",
+     }}
+     />
+    <br /><br />
+    <input
+    name="email"
+    placeholder="Email"
+    onChange={handleChange}
+    style={{width: "100%",
+    padding: "12px",
+    marginBottom: "20px",
+    borderRadius: "5px",
+    border: "1px solid gray",
+    boxSizing: "border-box",}}
+                
+ />
+            <br /><br />
+
+ <input
+   type="password"
+   name="password"
+   placeholder="Password"
+   onChange={handleChange}
+   style={{
+   width: "100%",
+   marginBottom: "20px",
+   borderRadius: "5px",
+   border: "1px solid gray",
+   boxSizing: "border-box",
+   padding: "12px",
+   }}
             />
             <br /><br />
-            <input
-                name="email"
-                placeholder="Email"
-                onChange={handleChange}
-            />
-            <br /><br />
-            <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={handleChange}
-            />
-            <br /><br />
-            <button onClick={register}>Register</button>
+<button onClick={register}
+  style={{
+  width: "100%",
+  marginBottom: "15px",
+  borderRadius: "50px",
+  border: "1px solid gray",
+  boxSizing: "border-box",
+  color:"white",
+  padding:"7px",
+  backgroundColor:"blue",
+  border:"none",
+  cursor:"pointer",
+            }}>
+                Register
+            </button>
         </div>
     );
 }
